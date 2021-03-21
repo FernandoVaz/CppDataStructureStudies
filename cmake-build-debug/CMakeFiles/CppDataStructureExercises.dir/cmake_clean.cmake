aa231093539f97d14ee0befde46aefa52e044f81
Fernando Vaz <contact@fernandomvaz.com>
2021-03-21 12:29:58 -0300

    Initial repo commit

---
+++ cmake-build-debug/CMakeFiles/CppDataStructureExercises.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CppDataStructureExercises.dir/main.cpp.obj"
+  "CppDataStructureExercises.exe"
+  "CppDataStructureExercises.exe.manifest"
+  "CppDataStructureExercises.pdb"
+  "libCppDataStructureExercises.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CppDataStructureExercises.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Welcome to the Data Structure bigO Notation viewer" << std::endl;
+    return 0;
+}
