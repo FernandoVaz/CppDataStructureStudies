e8391127bf2d3506426a56a1771b4818295e8fdc
Fernando Vaz <contact@fernandomvaz.com>
2021-03-21 13:28:36 -0300

    Project startup

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
 int main() {
+    int selector;
     std::cout << "Welcome to the Data Structure bigO Notation viewer" << std::endl;
+    std::cout << "Please select what to do next" << std::endl;
+    std::cout << "Type 1 to select the Wiki for information about data structure" << std::endl;
+    std::cout << "Type 2 to perform a linear search of X elements randomly generated" << std::endl;
+    std::cin >> selector;
+
     return 0;
 }
